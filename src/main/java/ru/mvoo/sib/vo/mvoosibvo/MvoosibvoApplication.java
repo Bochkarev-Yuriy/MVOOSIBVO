fd090d5746037587b99263f5f1093ed321f57494
Bochkarev Yuriy <bochkarev.yuriy.i@gmail.com>
2019-06-07 17:05:54 +0700

    init project

---
+++ src/main/java/ru/mvoo/sib/vo/mvoosibvo/MvoosibvoApplication.java
@@ -0,0 +1,13 @@
+package ru.mvoo.sib.vo.mvoosibvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvoosibvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvoosibvoApplication.class, args);
+	}
+
+}
