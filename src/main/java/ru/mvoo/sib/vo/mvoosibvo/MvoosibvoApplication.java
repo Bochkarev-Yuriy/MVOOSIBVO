271b468e21213a37f63def9b80f592e94adacfb5
lyubov <nemchukl@mail.ru>
2019-06-13 02:10:13 +0700

    Mvoosibvo-2 вывести hello world на jsp+mvc. страница-index.jsp

--- src/main/java/ru/mvoo/sib/vo/mvoosibvo/MvoosibvoApplication.java
+++ src/main/java/ru/mvoo/sib/vo/mvoosibvo/MvoosibvoApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MvoosibvoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MvoosibvoApplication.class, args);
 	}
-
 }
