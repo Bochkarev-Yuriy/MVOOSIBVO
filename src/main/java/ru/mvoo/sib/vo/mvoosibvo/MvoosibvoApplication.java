d819e79f36a061dfe751845d198bf29ad5922c39
lyubov <nemchukl@mail.ru>
2019-06-14 02:40:17 +0700

    mvoosibvo-8. отрефакторить весь код (поменять пробелы на таб), добавить пустые строки перед методами

--- src/main/java/ru/mvoo/sib/vo/mvoosibvo/MvoosibvoApplication.java
+++ src/main/java/ru/mvoo/sib/vo/mvoosibvo/MvoosibvoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MvoosibvoApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(MvoosibvoApplication.class, args);
 	}
