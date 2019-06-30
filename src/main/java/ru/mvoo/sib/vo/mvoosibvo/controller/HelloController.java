68b7a0c0c1e0c50738900db16d1240bcea67b7d7
lyubov <nemchukl@mail.ru>
2019-07-01 00:20:58 +0700

    mvoosibvo-10. создать нормализованную(первые 4 обязательны) бд для сущности адрес, заполнить бд тестовыми данными и прокинуть данные на index страницу.(решение кофликта + правка по ревью) https://trello.com/c/mwjrs1kl/10-mvoosibvo-10-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D1%82%D1%8C-%D0%BD%D0%BE%D1%80%D0%BC%D0%B0%D0%BB%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%83%D1%8E%D0%BF%D0%B5%D1%80%D0%B2%D1%8B%D0%B5-4-%D0%BE%D0%B1%D1%8F%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B-%D0%B1%D0%B4-%D0%B4%D0%BB%D1%8F-%D1%81%D1%83%D1%89%D0%BD%D0%BE%D1%81%D1%82%D0%B8-%D0%B0%D0%B4%D1%80%D0%B5%D1%81-%D0%B7%D0%B0%D0%BF%D0%BE%D0%BB%D0%BD%D0%B8%D1%82%D1%8C-%D0%B1%D0%B4-%D1%82%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D1%8B%D0%BC%D0%B8-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%B8-%D0%BF%D1%80%D0%BE%D0%BA%D0%B8%D0%BD%D1%83%D1%82%D1%8C-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5

--- src/main/java/ru/mvoo/sib/vo/mvoosibvo/controller/HelloController.java
+++ src/main/java/ru/mvoo/sib/vo/mvoosibvo/controller/HelloController.java
@@ -1,9 +1,11 @@
 package ru.mvoo.sib.vo.mvoosibvo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import ru.mvoo.sib.vo.mvoosibvo.service.abstr.AddressService;
 
 @Controller
 public class HelloController {
